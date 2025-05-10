595a27c56216b6eb4b29d4ff273f43a85efb660a
camposmm <mcamposdossantos@byupathway.edu>
2025-05-10 17:57:47 -0300

    01

--- server.js
+++ server.js
@@ -41,3 +41,25 @@ const host = process.env.HOST
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
+
+// Add this with your other routes
+app.get("/cse-motors", function(req, res){
+  res.render("cse-motors", {
+    title: "CSE Motors",
+    vehicle: {
+      name: "DMC Delorean",
+      features: ["3 Cup holders", "Superman doors", "Fuzzy dice"],
+      upgrades: [
+        ["Flux Capacitor", "Flame Breaks"],
+        ["32bit", "Bumper Stickers", "Hup Caps"]
+      ],
+      reviews: [
+        { text: "So fast it's almost like traveling in time.", rating: "4/5" },
+        { text: "Coolest ride on the road.", rating: "4/5" },
+        { text: "I'm feeling McFly!", rating: "5/5" },
+        { text: "The most futuristic ride of our day.", rating: "4.5/5" },
+        { text: "80's livin and I love it!", rating: "5/5" }
+      ]
+    }
+  });
+});
