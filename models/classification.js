f4ee83fb54c1037b2e08d8e062c553a6508c2a97
camposmm <mcamposdossantos@byupathway.edu>
2025-05-24 00:28:55 -0300

    week03

---
+++ models/classification.js
@@ -0,0 +1,8 @@
+// models/classification.js
+const pool = require('../database/');
+
+async function getClassifications() {
+  return await pool.query("SELECT * FROM public.classification ORDER BY classification_name");
+}
+
+module.exports = { getClassifications };
