56a280829515606bb09633192a4024756c742d74
camposmm <mcamposdossantos@byupathway.edu>
2025-06-01 11:24:55 -0300

    week04

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -5,11 +5,19 @@ const utilities = require("../utilities");
  *  Build login view
  * ************************** */
 async function buildLogin(req, res, next) {
-  let nav = await utilities.getNav();
+  let nav = ""; // Initialize nav as empty string
+  try {
+    nav = await utilities.getNav(); // Try to get navigation data
+  } catch (error) {
+    console.error("Error fetching navigation:", error.message); // Log error if DB fails
+    // Proceed without navigation data if DB connection fails
+  }
   res.render("account/login", {
     title: "Login",
-    nav,
+    nav, // Pass nav (either fetched data or empty string)
     errors: null,
+    message: req.flash("notice"), // Pass flash messages
+    messageType: req.flash("messageType") // Pass flash message type
   });
 }
 
